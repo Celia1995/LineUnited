69284eaa72a1711ee17f473bdbdf4f9ea4ea03e0
Celia <celi_1995@hotmail.com>
2019-11-27 12:14:26 +0100

    Subido proyecto de Unity

---
+++ Assets/Scripts/Cuadricula.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Cuadricula : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.CreatePrimitive(PrimitiveType.Cube);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
