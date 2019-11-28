78e4aa64949fed9180fc6b5589d09b4ba66429d9
Celia <celi_1995@hotmail.com>
2019-11-28 19:07:31 +0100

    Lineas

--- Assets/Scripts/Linea.cs
+++ Assets/Scripts/Linea.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Linea : MonoBehaviour
 {
     //SI TIENE UN COLLIDER QUE APAREZCA CON INVOKE
@@ -9,27 +10,71 @@ public class Linea : MonoBehaviour
     // bool si esta false, lo activamos su mesh renderer.
 
     //GameObject line;
-    Renderer r;
-    bool clickeada;
 
-    public void Awake()
+    //ejercicio de crear dos lineas formando una L al reves
+
+    public GameObject lineaHorizontal;
+    public GameObject lineaVertical;
+
+
+
+    void Start()
     {
-        r = GetComponent<Renderer>();
+        for (int i = 0; i < 10; i++) {
+            if (i != 9) { 
+                GameObject lineaV = Instantiate(lineaVertical, Vector3.zero, lineaVertical.transform.rotation);
+                lineaV.transform.position = transform.position + new Vector3(0.55f, 0, 0);
+                lineaV.name = "lineaV" + i;
+            }
+
+            GameObject lineaH = Instantiate(lineaHorizontal, Vector3.zero, lineaHorizontal.transform.rotation);
+            lineaH.transform.position = transform.position + new Vector3(0, -0.55f, 0);
+
+            lineaH.name = "lineaH" + i;
+
+            transform.position += Vector3.right+new Vector3(0.1f,0,0);
+        }
+
+
+        //Vector3 posicionVertical = Vector3.zero;
+        //lineaVertical.transform.position = posicionVertical;
+
+        //Vector3 posicionBorde=Vector3.zero;
+        //posicionBorde.x = (casillasX + (casillasX * offset)) / 2;
+        //posicionBorde.y = (-casillasY - (casillasY * offset)) / 2;
+        //bordeTemporal.transform.position = posicionBorde;
+
+
     }
 
-    private void OnMouseDown()
-    { 
-        clickeada = true;
-    }
 
-    private void OnMouseOver()
-    {
-        r.enabled = true;
-    }
+    //bordeTemporal = Instantiate(borde, Vector3.zero, Quaternion.identity);
 
-    private void OnMouseExit()
-    {
-        if (clickeada == false) 
-            r.enabled = false;
-    }
+
+
+
+
+    /* Renderer r;
+     bool clickeada;
+
+     public void Awake()
+     {
+         r = GetComponent<Renderer>();
+     }
+
+     private void OnMouseDown()
+     { 
+         clickeada = true;
+     }
+
+     private void OnMouseOver()
+     {
+         r.enabled = true;
+     }
+
+     private void OnMouseExit()
+     {
+         if (clickeada == false) 
+             r.enabled = false;
+     }*/
 }
