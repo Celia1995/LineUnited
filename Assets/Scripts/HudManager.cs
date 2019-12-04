716c00eef55af2d107621cde08a084c27371b363
jose <josegemas1999@gmail.com>
2019-12-04 12:36:06 +0100

    Auto stash before merge of "master" and "origin/master"
    cambio de escena a menu principal

--- Assets/Scripts/HudManager.cs
+++ Assets/Scripts/HudManager.cs
@@ -8,15 +8,19 @@ public class HudManager : MonoBehaviour
     public GameObject panelPausa;
     public TextMeshProUGUI timeCounterText;
     public GameObject botonContinuar;
+    public GameObject botonVolver;
+    public GameObject botonOpciones;
+    public GameObject panelOpciones;
+    public TMP_Text[] puntuaciones;
 
     private void Start()
     {
         SetActivePausePanel(false);
     }
 
-    public void SetActivePausePanel(bool valor)
+    public void SetActivePausePanel(bool value)
     {
-        panelPausa.SetActive(valor);
+        panelPausa.SetActive(value);
     }
 
     public void SetTimeCounter(float newTime)
@@ -24,5 +28,40 @@ public class HudManager : MonoBehaviour
         timeCounterText.text = newTime.ToString("f0");
     }
 
-    
+    public void MostrarBotonOpciones(bool value)
+    {
+       botonOpciones.SetActive(value);
+    }
+
+    public void MostrarPanelOpciones(bool value)
+    {
+        panelOpciones.SetActive(value);
+    }
+
+    public void MostrarBotonVolver(bool value)
+    {
+        botonVolver.SetActive(value);
+    }
+
+    public void AparecerPanelOpcioens(bool value)
+    {
+        SetActivePausePanel(!value);
+        panelOpciones.SetActive(value);
+    }
+
+    public void AparecerPausepanel(bool value)
+    {
+        MostrarPanelOpciones(!value);
+        panelPausa.SetActive(value);
+    }
+
+    public void EscribirPuntuacion(int equipo, int puntos)
+    {
+        puntuaciones[equipo].text = puntos.ToString("f0");
+    }
+
+    public void Salir()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Menu");
+    }
 }
